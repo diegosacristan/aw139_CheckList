a76a7797bd5b7ec833947f6dafc9a603f427eecb
4nthr@x83 <149607817+diegosacristan@users.noreply.github.com>
2026-03-04 10:43:37 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ﻿// Central version config for UI + SW registration.
 window.APP_CONFIG = Object.freeze({
-  APP_VERSION: '1.1.17',
-  CONTENT_VERSION: '2026.03.04-g',
+  APP_VERSION: '1.1.18',
+  CONTENT_VERSION: '2026.03.04-h',
   CHANGELOG_URL: './changelog.json'
 });
